c1ca74c5ced3bbfd42e8b9c6fbd3f21fc61d7994
Kukuh Sabrowi <qu2h_brow@yahoo.co.id>
2020-11-16 15:09:44 +0700

    add text editor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Editor } from '@tinymce/tinymce-react'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Editor
+         initialValue=""
+         apiKey='w0wselq9wnb0snnpcw34hkhxvs8dg13nwsl3d7639t911cfd'
+         init={{
+           height: 500,
+           menubar: true,
+           plugins: [
+             'advlist autolink lists link image charmap print preview anchor',
+             'searchreplace visualblocks code fullscreen',
+             'insertdatetime media table paste code help wordcount'
+           ],
+           toolbar:
+             'undo redo | formatselect | bold italic backcolor | \
+             alignleft aligncenter alignright alignjustify | \
+             bullist numlist outdent indent | removeformat | help'
+         }}
+       />
     </div>
   );
 }
