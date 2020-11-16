83448fba8ae0511f25692c84d12d89e929b2f239
Kukuh Sabrowi <qu2h_brow@yahoo.co.id>
2020-11-16 15:20:20 +0700

    fix toolbar

--- src/App.js
+++ src/App.js
@@ -16,10 +16,7 @@ function App() {
              'searchreplace visualblocks code fullscreen',
              'insertdatetime media table paste code help wordcount'
            ],
-           toolbar:
-             'undo redo | formatselect | bold italic backcolor | \
-             alignleft aligncenter alignright alignjustify | \
-             bullist numlist outdent indent | removeformat | help'
+           toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help'
          }}
        />
     </div>
